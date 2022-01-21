b46e2f85f222f9ecc742ec04feeb0306079ce310
xxxmkxxx <maks88648@gmail.com>
2022-01-21 16:14:59 +0400

    learn diff and diff --staged

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("check changes");
+        System.out.println("learning: git diff | --staged |");
     }
 }
 
